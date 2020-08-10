63a3209678759d02aa49105a2f8e45b8dec16aea
Eugene <eugenefromrus@gmail.com>
2020-08-11 09:29:45 +1000

    Возврат изменений из 33ae687

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,15 +2,9 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
-import dialogsReducer from "./dialogsReducer";
-import messageReducer from "./messagesReducers";
-import attachmentsReducer from "./attachments";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   profile: profileReducer,
-  dialogs: dialogsReducer,
-  message: messageReducer,
-  attachments: attachmentsReducer,
 });
