0fcd2cf09243da35c076e05c102c22ace9e84dda
Eugene <eugenefromrus@gmail.com>
2020-08-10 10:48:57 +1000

    checkpoint

--- client/src/components/messanger/pages/ChatPage/index.jsx
+++ client/src/components/messanger/pages/ChatPage/index.jsx
@@ -1,41 +1,34 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router';
-import { Messages, ChatInput, Status, Sidebar } from 'containers';
-import { connect } from 'react-redux';
+// import { Messages, ChatInput, Status, Sidebar } from 'containers';
+// import { connect } from 'react-redux';
 
 import './ChatPage.scss';
 
-import { dialogsActions } from 'redux/actions';
+// import { dialogsActions } from 'redux/actions';
 
 const ChatPage = props => {
-  const { setCurrentDialogId, user } = props;
-  useEffect(() => {
-    const { pathname } = props.location;
-    const dialogId = pathname.split('/').pop();
-    setCurrentDialogId(dialogId);
-  }, [props.location.pathname]);
+  // const { setCurrentDialogId, user } = props;
+  // useEffect(() => {
+  //   const { pathname } = props.location;
+  //   const dialogId = pathname.split('/').pop();
+  //   setCurrentDialogId(dialogId);
+  // }, [props.location.pathname]);
 
   return (
     <section className="chatpage">
       <div className="chat">
-        <Sidebar />
-        {user && (
-          <div className="chat__dialog">
-            <Status />
-            <Messages />
+        {/* <Sidebar /> */}
+        <div className="chat__dialog">
+            {/* <Status /> */}
+            {/* <Messages /> */}
             <div className="chat__dialog-input">
-              <ChatInput />
+              {/* <ChatInput /> */}
             </div>
           </div>
-        )}
       </div>
     </section>
   );
 };
 
-export default withRouter(
-  connect(
-    ({ user }) => ({ user: user.data }),
-    dialogsActions,
-  )(ChatPage),
-);
+export default ChatPage;
