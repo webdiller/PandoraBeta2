0bd189de11f44552ecd898e2687ebcf75d3f1f7c
Eugene <eugenefromrus@gmail.com>
2020-08-13 10:00:33 +1000

    Не законченные модальные окна

--- client/src/components/Layout/HeaderProfile.jsx
+++ client/src/components/Layout/HeaderProfile.jsx
@@ -1,82 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Modal, Button, Form } from 'react-bootstrap';
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { registeruser } from "../../actions/authActions";
 
-function HeaderProfile() {
+class HeaderProfile extends Component {
 
-    const [formData, setFormData] = React.useState({
-        registration_email: '',
-        auth_email: '',
-        auth_password: '',
-        forgot_email: ''
-    });
+    constructor() {
+        super();
+        this.state = {
+            showRegistration: false,
+            handleCloseRegistration: false,
+            handleShowRegistration: true,
 
-    const [errors, setErrors] = React.useState({
-        status: false,
-        message: ''
-    });
+            showAuth: false,
+            handleCloseAuth: false,
+            handleShowAuth: true,
 
-    const onChange = e => { setFormData({ ...formData, [e.target.name]: e.target.value }) }
+            showForgot: false,
+            handleCloseForgot: false,
+            handleShowForgot: true,
 
-    const onSubmit = e => {
-        e.preventDefault();
-        console.log(formData);
+            email: "",
+            errors: {},
+        };
+
+        // this.onChange = this.onChange.bind(this);
+        // this.onSubmit = this.onSubmit.bind(this);
     }
 
-    const [showRegistration, setShowRegistration] = React.useState(false);
-    const handleCloseRegistration = () => setShowRegistration(false);
-    const handleShowRegistration = () => setShowRegistration(true);
+    // const[showRegistration, setShowRegistration] = React.useState(false);
+    // const handleCloseRegistration = () => setShowRegistration(false);
+    // const handleShowRegistration = () => setShowRegistration(true);
 
-    const [showAuth, setShowAuth] = React.useState(false);
-    const handleCloseAuth = () => setShowAuth(false);
-    const handleShowAuth = () => setShowAuth(true);
+    // const[showAuth, setShowAuth] = React.useState(false);
+    // const handleCloseAuth = () => setShowAuth(false);
+    // const handleShowAuth = () => setShowAuth(true);
 
-    const [showForgot, setShowForgot] = React.useState(false);
-    const handleCloseForgot = () => setShowForgot(false);
-    const handleShowForgot = () => setShowForgot(true);
+    // const[showForgot, setShowForgot] = React.useState(false);
+    // const handleCloseForgot = () => setShowForgot(false);
+    // const handleShowForgot = () => setShowForgot(true);
 
-    const registrationHandle = (e, url = "http://localhost:5000/api/users/register") => {
-        e.preventDefault();
-        fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: formData.registration_email,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            .then(response => response.json())
-            .then(json => console.log(json))
-    }
 
-    const authHandle = (e, url = "http://localhost:5000/api/users/login") => {
-        e.preventDefault();
-        fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: formData.auth_email,
-                password: formData.auth_password,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            .then(data => data.json())
-            .then(data => console.log(data))
-    }
-
-    if (1) {
+    render() {
         return (
             <React.Fragment>
-                <Button variant="primary" onClick={handleShowRegistration} className="header__bar-icon">
+
+                <Button variant="primary"
+                    // onClick={handleShowRegistration}
+                    className="header__bar-icon">
                     <i className="header__bar-icon-inner far fa-user"></i>
                 </Button>
 
                 {/* Регистрация */}
                 <Modal
-                    show={showRegistration}
-                    onHide={handleCloseRegistration}
+                    // show={true}
+                    // onHide={handleCloseRegistration}
                     backdrop="static"
                     keyboard={false}
                 >
@@ -85,43 +66,51 @@ function HeaderProfile() {
                     </Modal.Header>
 
                     <Modal.Body>
-                        <Form onSubmit={e => registrationHandle(e)} className="d-flex flex-column">
+                        <Form className="d-flex flex-column">
                             <Form.Group controlId="registration">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control onChange={e => { onChange(e) }} name="registration_email" type="email" placeholder="Enter email" />
+                                <Form.Control value={this.state.email}
+                                    onChange={this.onChange} type="email" placeholder="Enter email" />
                                 <Form.Text className="text-muted">
                                     We'll never share your email with anyone else.
-                                </Form.Text>
+                                    </Form.Text>
                             </Form.Group>
 
                             <Button className="ml-auto" variant="primary" type="submit">
                                 Submit
-                            </Button>
+                                </Button>
                         </Form>
                     </Modal.Body>
 
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={() => {
-                            handleCloseRegistration();
-                            handleShowForgot();
-                        }}>
+                        <Button variant="secondary"
+                        // onClick={() => {
+                        // handleCloseRegistration();
+                        // handleShowForgot();
+                        // }}
+                        >
                             Забыли пароль?
-                        </Button>
+                            </Button>
 
-                        <Button variant="secondary" onClick={handleCloseRegistration}>
+                        <Button variant="secondary"
+                        //  onClick={
+                        // handleCloseRegistration
+                        // }
+                        >
                             Close
-                        </Button>
+                            </Button>
 
-                        <Button onClick={() => {
-                            handleCloseRegistration()
-                            handleShowAuth()
-                        }} variant="primary">Уже есть аккаунт?</Button>
+                        <Button
+                            // onClick={() => {
+                            //     handleCloseRegistration()
+                            //     handleShowAuth()
+                            // }}
+                            variant="primary">Уже есть аккаунт?</Button>
                     </Modal.Footer>
                 </Modal>
 
-
                 {/* Забыли пароль? */}
-                <Modal
+                {/* <Modal
                     show={showForgot}
                     onHide={handleCloseForgot}
                     backdrop="static"
@@ -132,33 +121,33 @@ function HeaderProfile() {
                     </Modal.Header>
 
                     <Modal.Body>
-                        <Form onSubmit={e => onSubmit(e)} className="d-flex flex-column">
+                        <Form className="d-flex flex-column">
 
                             <Form.Group controlId="forgot">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control onChange={e => { onChange(e) }} name="forgot_email" type="email" placeholder="Enter email" />
+                                <Form.Control name="forgot_email" type="email" placeholder="Enter email" />
                                 <Form.Text className="text-muted">
                                     Введите вашу почту
-                                </Form.Text>
+                                    </Form.Text>
                             </Form.Group>
 
                             <Button className="ml-auto" variant="primary" type="submit">
                                 Submit
-                            </Button>
+                                </Button>
                         </Form>
                     </Modal.Body>
 
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleCloseForgot}>
                             Close
-                        </Button>
+                            </Button>
 
                         <Button variant="primary">Understood</Button>
                     </Modal.Footer>
-                </Modal>
+                </Modal> */}
 
                 {/* Авторизация */}
-                <Modal
+                {/* <Modal
                     show={showAuth}
                     onHide={handleCloseAuth}
                     backdrop="static"
@@ -169,54 +158,53 @@ function HeaderProfile() {
                     </Modal.Header>
 
                     <Modal.Body>
-                        <Form onSubmit={e => authHandle(e)} className="d-flex flex-column">
+                        <Form className="d-flex flex-column">
 
                             <Form.Group controlId="auth">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control onChange={e => { onChange(e) }} name="auth_email" type="email" placeholder="Enter email" />
+                                <Form.Control name="auth_email" type="email" placeholder="Enter email" />
                                 <Form.Text className="text-muted">
                                     We'll never share your email with anyone else.
-                                </Form.Text>
+                                    </Form.Text>
                             </Form.Group>
 
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control onChange={e => { onChange(e) }} name="auth_password" type="password" placeholder="Password" />
+                                <Form.Control name="auth_password" type="password" placeholder="Password" />
                             </Form.Group>
 
                             <Button className="ml-auto" variant="primary" type="submit">
                                 Submit
-                            </Button>
+                                </Button>
                         </Form>
                     </Modal.Body>
 
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleCloseAuth}>
                             Close
-                        </Button>
+                            </Button>
 
                         <Button variant="primary">Understood</Button>
                     </Modal.Footer>
-                </Modal>
+                </Modal> */}
+
             </React.Fragment >
         )
-
-
-    } else {
-        return (
-            <Link to="/profile" className="header__bar-icon">
-                <i className="header__bar-icon-inner far fa-user"></i>
-            </Link>
-        )
     }
+
+
 }
 
-const mapStateToProps = () => {
-    return {};
-}
 
-const mapDispatchToProps = () => {
-    return {};
-}
+HeaderProfile.propTypes = {
+    registeruser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired,
+};
 
-export default HeaderProfile;
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors,
+});
+
+export default connect(mapStateToProps, { registeruser })(withRouter(HeaderProfile));
