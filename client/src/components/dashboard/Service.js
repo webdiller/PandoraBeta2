b5044d786498a572e0f091520cba67585d0e61bb
Eugene <eugenefromrus@gmail.com>
2020-08-21 16:39:32 +1000

    checkpoint

--- client/src/components/dashboard/Service.js
+++ client/src/components/dashboard/Service.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 // import Moment from "react-moment";
 import { deleteService } from "../../actions/profileActions";
+import './Service.scss';
 
 class Service extends Component {
   onDeleteClick(id) {
@@ -14,18 +15,16 @@ class Service extends Component {
     // if (this.props.services === null) {
     //   <div>Null</div>;
     // } else {
+
     const services = this.props.services.map((s) => (
-      <div key={s._id}>
-        <h1>{s.title}</h1>
-        <p>{s.categories}</p>
-        <h4>{s.content}</h4>
+      <div className="services__item mb-2" key={s._id}>
+        <h1 className="services__title">Заголовок: {s.title}</h1>
+        <h4 className="services__description">Описание: {s.content}</h4>
+        <p className="services__categories">Категории: {s.categories}</p>
         <div>
           <button
             onClick={this.onDeleteClick.bind(this, s._id)}
-            className="btn btn-danger"
-          >
-            x
-          </button>
+            className="services__delete">x</button>
         </div>
       </div>
     ));
@@ -34,17 +33,7 @@ class Service extends Component {
     return (
       <div>
         <h4 className="mb-4">Услуги</h4>
-        <div className="table">
-          <div>
-            <div>
-              <p>Название услуги</p>
-              <p>Категории</p>
-              <p>Описание услуги</p>
-              <p></p>
-            </div>
-          </div>
-          <div>{services}</div>
-        </div>
+        <div className="services">{services}</div>
       </div>
     );
   }
