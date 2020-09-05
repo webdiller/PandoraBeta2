818ba8741d80a0d6afe34ea39e58bced004272bc
Eugene <eugenefromrus@gmail.com>
2020-09-05 19:19:46 +1000

    Изменен стиль кнопки выхода из приложения

--- client/src/components/edit-profile/LogoutBtn.jsx
+++ client/src/components/edit-profile/LogoutBtn.jsx
@@ -4,8 +4,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { clearCurrentProfile } from "../../actions/profileActions";
-import Register from "../auth/Register";
-import Login from "../auth/Login";
 
 class Navbar extends Component {
     onLogoutClick(e) {
@@ -16,37 +14,19 @@ class Navbar extends Component {
 
     render() {
         const { isAuthenticated, user } = this.props.auth;
-
+        
         const authLinks = (
-            <li className="nav-item">
-                <a
-                    href=""
-                    onClick={this.onLogoutClick.bind(this)}
-                    className="nav-link"
-                >
-                    {/* <img
-            className="rounded-circle"
-            src={user.avatar}
-            alt={user.name}
-            style={{ width: "25px", marginRight: "5px" }}
-            title="You must have a Gravatar connected to your email to display an image"
-          />{" "} */}
-          Logout
-        </a>
-            </li>
+            <button
+                type="submit" 
+                className="site-btn site-btn_red site-btn_s1 mb-3"
+                onClick={this.onLogoutClick.bind(this)}>
+                Выйти из аккаунта
+            </button>
         );
 
         return (
             <div>
-                <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-                    <div className="container">
-
-                        <div className="collapse navbar-collapse" id="mobile-nav">
-
-                            {isAuthenticated ? authLinks : null}
-                        </div>
-                    </div>
-                </nav>
+                {isAuthenticated ? authLinks : null}
             </div>
         );
     }
